f4e51fabde67437f6f0d4862ff3000671e54ff00
Lupova19 <nadiqlupova@gmail.com>
2023-06-17 13:04:21 +0300

    first commit

---
+++ BooksInTheLibrary/Book.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksInTheLibrary
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int Year { get; set; }
+        public int CatalogNumber { get; set; }
+
+        private List<Book> books;
+
+        public Book()
+        {
+            books = new List<Book>();
+        }
+
+
+        public Book(string title, string author, string publisher, int year, int catalogNumber)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Publisher = publisher;
+            this.Year = year;
+            this.CatalogNumber = catalogNumber;
+        }
+
+        //а
+        public void AddBook(string title, string author, string publisher, int year, int catalogNumber)
+        {
+            Book book = new Book(title, author, publisher, year, catalogNumber);
+            books.Add(book);
+            Console.WriteLine("Книгата е добавена успешно!");
+        }
+        //б
+        public void DisplayBooks()
+        {
+            foreach (Book item in books)
+            {
+                Console.WriteLine($"Заглавие:{item.Title} | Автор:{item.Author} | Издател:{item.Publisher} | Година:{item.Year} | Номер в каталога:{item.CatalogNumber}");
+            }
+        }
+        //в
+        public void SearchByTitle(string title)
+        {
+            foreach (Book item in books)
+            {
+                if (item.Title == title)
+                {
+                    Console.WriteLine($"Заглавие: {item.Title}");
+                    Console.WriteLine($"Автор: {item.Author}");
+                    Console.WriteLine($"Издател: {item.Publisher}");
+                    Console.WriteLine($"Година: {item.Year}");
+                    Console.WriteLine($"Номер в каталога: {item.CatalogNumber}");
+                    Console.WriteLine();
+                    return;
+                }
+            }
+            Console.WriteLine("Няма въведени книги с това заглавие!");
+        }
+        // г
+        public void SearchByAuthor(string author)
+        {
+            foreach (Book item in books)
+            {
+                if (item.Author == author)
+                {
+                    Console.WriteLine($"Заглавие: {item.Title}");
+                    Console.WriteLine($"Автор: {item.Author}");
+                    Console.WriteLine($"Издател: {item.Publisher}");
+                    Console.WriteLine($"Година: {item.Year}");
+                    Console.WriteLine($"Номер в каталога: {item.CatalogNumber}");
+                    Console.WriteLine();
+                    return;
+                }
+            }
+            Console.WriteLine("Няма въведени книги с този автор!");
+        }
+        //д
+        public void UpdateInformationAboutBook(string title)
+        {
+            foreach (Book item in books)
+            {
+                if (item.Title == title)
+                {
+                    Console.Write("Въведи ново заглавие: ");
+                    item.Title = Console.ReadLine();
+                    Console.Write("Въведи нов автор: ");
+                    item.Author = Console.ReadLine();
+                    Console.Write("Въведи нов издател: ");
+                    item.Publisher = Console.ReadLine();
+                    Console.Write("Въведи нова година: ");
+                    item.Year = int.Parse(Console.ReadLine());
+                    Console.Write("Въведи нов номер в каталога: ");
+                    item.CatalogNumber = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Книгата е актуализирана успешно!");
+                    return;
+                }
+            }
+            Console.WriteLine("Няма въведени книги с това заглавие!");
+        }
+        //е
+        public void DeleteBook(string title)
+        {
+            int countOfBook = books.Count;
+            books.RemoveAll(book => book.Title == title);
+
+            if (books.Count < countOfBook)
+            {
+                Console.WriteLine("Книгата е изтрита успешно!");
+            }
+            else
+            {
+                Console.WriteLine("Няма въведени книги с това заглавие!");
+            }
+        }
+        //ж
+        public double AverageYear()
+        {
+            if (books.Count == 0)
+            {
+                return 0;
+            }
+
+            double averageYear = books.Average(book => book.Year);
+            return averageYear;
+        }
+        //з
+        public Book OldestBook()
+        {
+            if (books.Count == 0)
+            {
+                return null;
+            }
+
+            Book oldestBook = books.OrderBy(book => book.Year).First();
+            return oldestBook;
+        }
+        //и
+        public Book NewestBookByAuthor(string author)
+        {
+            Book newestBook = books.Where(book => book.Author == author)
+                                   .OrderByDescending(book => book.Year)
+                                   .FirstOrDefault();
+            return newestBook;
+        }
+        //й
+        public void SortByAuthorAndCatalogNumber()
+        {
+            books = books.OrderBy(book => book.Author)
+                         .ThenBy(book => book.CatalogNumber)
+                         .ToList();
+        }
+    }
+}
---
+++ BooksInTheLibrary/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksInTheLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book library = new Book();
+            int command = 0;
+
+            while (command != 11)
+            {
+                Console.WriteLine("1 - Добави книга");
+                Console.WriteLine("2 - Извади цялата информация за книгите");
+                Console.WriteLine("3 - Търсене на книга по заглавие");
+                Console.WriteLine("4 - Търсене на книга по автор");
+                Console.WriteLine("5 - Актуализиране на книга");
+                Console.WriteLine("6 - Изтриване на книга");
+                Console.WriteLine("7 - Изчисляване на средната година на книгите");
+                Console.WriteLine("8 - Извеждане на най-старата книга");
+                Console.WriteLine("9 - Извеждане на най-новата книга от автор");
+                Console.WriteLine("10 - Сортиране на книгите по автор и номер в каталога");
+                Console.WriteLine("11 - Изход от програмата");
+                Console.Write("Въведи команда: ");
+                command = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (command)
+                {
+
+                    case 1:
+                        Console.Write("Въведи заглавие: ");
+                        string title = Console.ReadLine();
+                        Console.Write("Въведи автор: ");
+                        string author = Console.ReadLine();
+                        Console.Write("Въведи издател: ");
+                        string publisher = Console.ReadLine();
+                        Console.Write("Въведи година: ");
+                        int year = int.Parse(Console.ReadLine());
+                        Console.Write("Въведи номер в каталога: ");
+                        int catalogNumber = int.Parse(Console.ReadLine());
+                        library.AddBook(title, author, publisher, year, catalogNumber);
+                        Console.WriteLine();
+                        break;
+
+                    case 2:
+                        library.DisplayBooks();
+                        break;
+
+                    case 3:
+                        Console.Write("Въведи заглавие: ");
+                        string searchTitle = Console.ReadLine();
+                        library.SearchByTitle(searchTitle);
+                        Console.WriteLine();
+                        break;
+
+                    case 4:
+                        Console.Write("Въведи автор: ");
+                        string searchAuthor = Console.ReadLine();
+                        library.SearchByAuthor(searchAuthor);
+                        Console.WriteLine();
+                        break;
+
+                    case 5:
+                        Console.Write("Въведи заглавие на книгата за да се актуализира: ");
+                        string updateTitle = Console.ReadLine();
+                        library.UpdateInformationAboutBook(updateTitle);
+                        Console.WriteLine();
+                        break;
+
+                    case 6:
+                        Console.Write("Въведи заглавие на книгата за да се изтрие: ");
+                        string deleteTitle = Console.ReadLine();
+                        library.DeleteBook(deleteTitle);
+                        Console.WriteLine();
+                        break;
+
+                    case 7:
+                        double averageYear = library.AverageYear();
+                        Console.WriteLine($"Средна година на книгите: {averageYear}");
+                        Console.WriteLine();
+                        break;
+
+                    case 8:
+                        Book oldestBook = library.OldestBook();
+                        if (oldestBook != null)
+                        {
+                            Console.WriteLine($"Най-стара книга: {oldestBook.Title}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Няма въведени книги в библиотеката!");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 9:
+                        Console.Write("Въведи автор за да се изведе най-новата книга: ");
+                        string authorNewestBook = Console.ReadLine();
+                        Book newestBook = library.NewestBookByAuthor(authorNewestBook);
+                        if (newestBook != null)
+                        {
+                            Console.WriteLine($"Най-новата книга от {authorNewestBook} е {newestBook.Title}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Няма въведени книги от {authorNewestBook} в библиотеката!");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 10:
+                        library.SortByAuthorAndCatalogNumber();
+                        Console.WriteLine("Сортираните книги по автор и номер в каталога:");
+                        library.DisplayBooks();
+                        break;
+                    case 11:
+                        Console.WriteLine("Изход от програмата!");
+                        break;
+                }
+
+            }
+        }
+    }
+}
